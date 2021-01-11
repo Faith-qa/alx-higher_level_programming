424019429e1ef1eadc6281575d86badc58fe4fd6
Faith-qa <atienofaith12@gmail.com>
2021-01-11 18:28:51 +0300

    defining a rectangle based on 0-rectangle.py

---
+++ 0x08-python_more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""defining a rectangle based on 0-rectangle.py"""
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, width):
+        self.__width = width
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, height):
+        self.__height = height
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >=0')
+        self.__height = height
+        
