ef419eb8133360cae58bf48f6c4de32ee1e3ee86
Faith-qa <atienofaith12@gmail.com>
2021-01-12 14:53:33 +0300

    corrected errors

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-"""defining a rectangle based on 1-rectangle.py"""
+"""defining a rectangle based on 1-rectangle.py
+"""
 
 
 class Rectangle:
+    """rectangle object with getter and setter
+    """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         return self.__width
 
     @width.setter
-    def width(self, width):
-        self.__width = width
-        if not isinstance(width, int):
+    def width(self, value):
+        self.__width = value
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        if width < 0:
+        elif value  < 0:
             raise ValueError('width must be >= 0')
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
         return self.__height
 
     @height.setter
-    def height(self, height):
-        self.__height = height
-        if not isinstance(height, int):
+    def height(self, value):
+        self.__height = value
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        if height < 0:
+        if value < 0:
             raise ValueError('height must be >=0')
-        self.__height = height
+        self.__height = value
 
     def area(self):
         area = self.__width * self.__height
