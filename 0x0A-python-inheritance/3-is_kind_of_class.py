a3b6751f60c119437ec186503ac3be91092ea529
Faith-qa <atienofaith12@gmail.com>
2021-01-18 22:04:32 +0300

    returns true if an object is an instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Class Inheritance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """isinstance
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
