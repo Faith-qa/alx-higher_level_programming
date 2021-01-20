d2b856e8195481119f2b91052df3d023cb2b9185
Faith-qa <atienofaith12@gmail.com>
2021-01-20 20:50:56 +0300

    class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+returns a json obj
+"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with sample data structures
+    """
+    return ob.__dict__
