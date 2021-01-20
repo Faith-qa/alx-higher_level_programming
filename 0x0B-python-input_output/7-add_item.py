c8f17dd4321dc5f87a04f3b4c7dfc847f87ef651
Faith-qa <atienofaith12@gmail.com>
2021-01-20 20:45:33 +0300

    load, add save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+import modules and sys function
+"""
+from sys import argv
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    j_list = load_from_json_file('add_item.json')
+except:
+    j_list = []
+
+for i in range(1, len(argv)):
+    j_list.append(argv[i])
+
+save_to_json_file(j_list, 'add_item.json')
