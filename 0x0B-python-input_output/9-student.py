0af276040722ae8dc14fbb202accd92b2a49261f
Faith-qa <atienofaith12@gmail.com>
2021-01-20 20:54:33 +0300

    class student to json

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+class student
+"""
+
+
+class Student():
+    """
+    initiate the public instance
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves dictionary representation of a student"""
+        return self.__dict__
