75aa5c361bf7a25e3ab12a12f9337ff674adbea4
Faith-qa <atienofaith12@gmail.com>
2021-01-20 20:03:36 +0300

    reads a text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+read from a file
+"""
+
+
+def read_file(filename=""):
+    """reads the whole file """
+    with open(filename, encoding='utf-8') as a_file:
+        print(a_file.read(), end="")
