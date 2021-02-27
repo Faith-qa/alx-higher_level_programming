1601a3aab5f7d3425a6adec34a2ac43136b2553b
Faith-qa <atienofaith12@gmail.com>
2021-02-27 10:36:17 +0300

    corrected errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         if total == 2:
-            print("{:d} arguments:".format(total-1))
+            print("{:d} argument:".format(total-1))
         else:
             print("{:d} arguments:".format(total-1))
         for i in range(1, total):
