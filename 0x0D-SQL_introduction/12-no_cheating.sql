cab6e0c6ac80b9c8c5e0463e2e9b4d4d912bc18c
Faith-qa <atienofaith12@gmail.com>
2021-03-04 12:16:57 +0300

    changes

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,5 @@
--- Update Bob score to 10
-UPDATE 'second_table' SET score = 10
-WHERE 'second_table'.'name' = 'Bob';
+-- Updates the score of Bob to 10 in the table second_table.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
 
