e1b240e219dc2fb232f4903098c9107917db98f9
Faith-qa <atienofaith12@gmail.com>
2021-03-19 21:05:11 +0300

    assign args and kwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -103,3 +103,16 @@ class Rectangle(Base):
         print('\n' * self.y, end='')
         for i in range(self.height):
             print(' ' * self.x + '#' * self.width)
+
+    def update(self, *args, **kwargs):
+        """
+        update rectangle attributes
+        """
+        expect = (self.id, self.width, self.height, self.x, self.y)
+        if args != ():
+            self.id, self.width, self.height, self.x, self.y,  = \
+                args + expect[len(args):len(expect)]
+
+        elif kwargs:
+            for(name, value) in kwargs.items():
+                setattr(self, name, value)
