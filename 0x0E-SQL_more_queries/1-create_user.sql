b1d261536010259a7c8496238a26b1c6ca21aeca
Faith-qa <atienofaith12@gmail.com>
2021-03-22 23:44:21 +0300

    fixed errors

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+----creates and new_user and grants in all privilages
+CREATE USER IF NOT EXISTS  'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_od_1_pwd';
+GRANT ALL PRIVILEGES ON *.*
+T0 'user_0d_1'@'localhost';
+FLUSH PROVILEGES
+
+
