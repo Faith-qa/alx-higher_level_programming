e499f19e915dd32430ce3c23411ab98054150bc5
Faith-qa <atienofaith12@gmail.com>
2021-03-23 21:21:31 +0300

    semi-standard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 class Square extends require('./5-square.js') {
-
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-          console.log(c.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     }
   }
