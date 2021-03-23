4d9c1e52f169dfcc4af1a1b679708b20868892bb
Faith-qa <atienofaith12@gmail.com>
2021-03-23 21:16:25 +0300

    adding an instance

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -10,7 +10,7 @@ class Square extends require('./4-rectangle.js') {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+          console.log(c.repeat(this.width));
       }
     }
   }
