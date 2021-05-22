67b4653cfa489d9b9802304900086243248c680e
Faith-qa <atienofaith12@gmail.com>
2021-05-22 21:33:16 +0300

    corrected errors

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
 """adds 2 integers"""
 
-def add_integer(a,b=98):
+
+def add_integer(a, b=98):
     """
     returns the sum of 2 integers
     """
     for i in (a, b):
         if i == a:
-            if isinstance(i, (int, float)):
+            if isinstance(i, int):
+                a = i
+            elif isinstance(i, float):
                 a = int(i)
             else:
                 raise TypeError("a must be an integer")
         if i == b:
-            if isinstance(i, (int, float)):
+            if isinstance(i, int):
+                b = i
+            elif isinstance(i, float):
                 b = int(i)
             else:
                 raise TypeError("b must be an integer")
-    
-    return a + b
+
+    return (int(a) + int(b))
