534a79213c3db59483a07008be61297f75c7dfe9
Faith-qa <atienofaith12@gmail.com>
2021-05-26 07:54:50 +0300

    changes

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- create database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- create user if it does not exist
+-- create user if it does not exist s
 CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grant SELECT previlages to user 2
