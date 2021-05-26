e1b16265c53a160cc7abb219e7718ce60f3d9f05
Faith-qa <atienofaith12@gmail.com>
2021-05-26 07:34:23 +0300

    fix errors

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
-----creates and new_user and grants in all privilages
-CREATE USER IF NOT EXISTS  'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_od_1_pwd';
+-- Create user 'user_0d_1' with all privileges
+-- Password for user set as 'user_0d_1_pwd'
+-- If user already exists, script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.*
-T0 'user_0d_1'@'localhost';
-FLUSH PROVILEGES
-
-
+TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
