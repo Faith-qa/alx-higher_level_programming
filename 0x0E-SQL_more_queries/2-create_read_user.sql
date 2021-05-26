218a4d097e75a174dfc6b9f678ce83739f3ab147
Faith-qa <atienofaith12@gmail.com>
2021-05-26 07:49:33 +0300

    corrected errors

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- create database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- create user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- grant SELECT previlages to user 2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
