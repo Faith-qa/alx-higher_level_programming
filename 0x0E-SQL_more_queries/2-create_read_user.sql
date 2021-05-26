5312807eae7b71a0e19caf07262695fec442e977
Faith-qa <atienofaith12@gmail.com>
2021-05-26 07:56:50 +0300

    changes

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,12 @@
--- create database if it does not exist
+-- Create database 'hbtn_0d_2'
+-- If database already exists, script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- create user if it does not exist s
-CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
+-- Create user 'user_0d_2'
+-- User password should be set to 'user_0d_2_pwd'
+-- If user already exists, script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- grant SELECT previlages to user 2
+-- User 'user_0d_2' should have only SELECT privilege in db 'hbtn_0d_2'
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
