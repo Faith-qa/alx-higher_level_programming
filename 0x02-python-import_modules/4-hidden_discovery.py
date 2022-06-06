793f68977c4ccc0ae02097a9dd8859fb6125c543
Faith-qa <atienofaith12@gmail.com>
2022-06-06 06:53:33 +0300

    changes

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from . import hidden_4
-    all_dir = dir(hidden_4)
-    for i in range(0, len(all_dir)):
-        if "__" != all_dir[i][:2]:
-            print(all_dir[i])
+    import hidden_4
+    for word in dir(hidden_4):
+        if word[1] != '_':
+            print(word)
